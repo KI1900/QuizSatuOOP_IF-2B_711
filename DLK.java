c86c60bc6a5924a86f2a182e0949bcf6393af369
RIFKIHIDAYATULLAHSETYONO <rifkihs26@gmail.com>
2026-05-04 14:14:09 +0700

    Clean up comments in DLK.java
    Removed comments and constructor details from DLK.java

--- DLK.java
+++ DLK.java
@@ -1,7 +1,1 @@
-Kendaraan(noPolisi, merek, tahunProduksi, statusServis)
-Mobil(noPolisi, merek, tahunProduksi, statusServis, jumlahPintu, tipeTransmisi)
-Motor(noPolisi, merek, tahunProduksi, statusServis, jenisMesin, kapasitasTangki)
-Dengan constructor:
 
-objek tidak pernah dibuat tanpa noPolisi, merek, tahunProduksi
-statusServis dapat langsung divalidasi menjadi hanya menunggu, diproses, atau selesai
